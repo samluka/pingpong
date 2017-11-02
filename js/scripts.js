def6fa0dda062cd3ae2b011c48d5040d70239228
samuel luka <samluka99@gmail.com>
2017-11-02 15:39:31 +0300

    randomized the result output

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business Logic.
+
 
 
 function checkDivisibility(number, collection) {
@@ -22,11 +22,11 @@ function pingPong(number) {
   'use strict';
   var i, resultArray = [];
 
-  if (number > 0) { // Positive numbers
+  if (number > 0) {
     for (i = 1; i <= number; i++) {
       resultArray = checkDivisibility(i, resultArray);
     }
-  } else if (number < 0) { // Negative numbers
+  } else if (number < 0) {
     for (i = -1; i >= number; i--) {
       resultArray = checkDivisibility(i, resultArray);
     }
@@ -36,53 +36,28 @@ function pingPong(number) {
 
   return resultArray;
 }
-var count = 0; 
+
+var count = 0;
 function placeNumber(number) {
-  'use strict';
-  var bodyWidth = $(".page-wrapper").width(),
-    bodyHeight = $(".page-wrapper").height(),
-    // Subtract 100px from width and height so it stays within the page
-    randPosX = Math.floor((Math.random() * (bodyWidth - 100))),
-    randPosY = Math.floor((Math.random() * (bodyHeight - 100))),
-    // We need to identify each number item with a unique ID
-    newDiv = '<div class="random-item" id="number-' + count + '" >' + number + '</div>',
-    randColor = "#" + Math.floor(Math.random() * 16777215).toString(16),
-    randFontSize = (Math.random() * 10).toFixed(2).toString() + "rem",
-    styles = {
-      'left': randPosX,
-      'top': randPosY,
-      'color': randColor,
-      'font-size': randFontSize
-    };
+ 'use strict';
+ var bodyWidth = $(".page-wrapper").width(),
+   bodyHeight = $(".page-wrapper").height(),
 
-  $("#main").append(newDiv);
+   randPosX = Math.floor((Math.random() * (bodyWidth - 100))),
+   randPosY = Math.floor((Math.random() * (bodyHeight - 100))),
 
-  $("#number-" + count).css(styles);
-  count++;
+   newDiv = '<div class="random-item" id="number-' + count + '" >' + number + '</div>',
+   randColor = "#" + Math.floor(Math.random() * 16777215).toString(16),
+   randFontSize = (Math.random() * 10).toFixed(2).toString() + "rem",
+   styles = {
+     'left': randPosX,
+     'top': randPosY,
+     'color': randColor,
+     'font-size': randFontSize
+   };
+
+ $("#main").append(newDiv);
+
+ $("#number-" + count).css(styles);
+ count++;
 }
-
-$(document).ready(function () {
-  'use strict';
-  $("form#ping-pong").submit(function (event) {
-    event.preventDefault();
-    $("ul#result").empty();
-    var inputNumber = parseInt($("input#number").val()),
-      numbers = pingPong(inputNumber);
-
-    // To prevent all the numbers showing up at once, we show them in a step by step fashion
-    function showNumbers(index, time) {
-      if (index < numbers.length) {
-        setTimeout(function () {
-          placeNumber(numbers[index]);
-          // $("ul#result").append("<li>" + numbers[index] + "</li>");
-          showNumbers(++index, time / index);
-        }, time);
-      }
-    }
-    showNumbers(0, 1000);
-  });
-
-  $("button#clear").click(function () {
-    $(".random-item").remove();
-  });
-});
